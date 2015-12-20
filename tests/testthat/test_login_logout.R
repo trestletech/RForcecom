ea14f4303e5d83d72d43a7abf1e7163c3903f16c
Steven Mortimer <reportmort@gmail.com>
2015-12-19 20:12:00 -0500

    Expand Test Case Coverage

--- tests/testthat/test_login_logout.R
+++ tests/testthat/test_login_logout.R
@@ -6,11 +6,17 @@ test_that("Checking login and logout", {
   password <- Sys.getenv("RFORCECOM_PASSWORD")
   session <- NULL
   
-  # test login
+  # test successful login
   tryCatch(session <- rforcecom.login(username, password))
   expect_true(length(session) > 0)
   
-  # test logout
+  # test successful logout
   tryCatch(logout_result <- rforcecom.logout(session))
   expect_true(logout_result)
+  
+  # test unsuccessful login
+  expect_error(rforcecom.login(username='FailingTest', password='WrongPassword'))
+  
+  # test unsuccessful logout
+  expect_error(rforcecom.logout(session='FailingTest'))
 })
